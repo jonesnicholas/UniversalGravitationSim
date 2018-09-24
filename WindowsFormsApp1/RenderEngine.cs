b88fb62d1454436be5537e40c93a11ac25bba689
Nick Jones <nijon@microsoft.com>
2018-09-24 11:52:00 -0700

    starting code to render Body Info Box

--- WindowsFormsApp1/RenderEngine.cs
+++ WindowsFormsApp1/RenderEngine.cs
@@ -83,6 +83,20 @@ namespace WindowsFormsApp1
             Vector windowPosition = pOffset * scale + baseWindowOffset;
             //renderCircle(dc, pen, windowPosition, Math.Max(Math.Pow(body.m, 1.0 / 3.0), 0.25) * scale);
             renderCircle(dc, pen, windowPosition, body.r * scale);
+            //renderBodyInfoBox(dc, body, windowPosition);
+        }
+
+        internal void renderBodyInfoBox(Graphics dc, Body body, Vector Position)
+        {
+            Pen InfoPen = new Pen(Color.Blue, 1);
+            float lineLength = 20;
+            PointF Center = new PointF((float)Position.x, (float)Position.y);
+            PointF TopLeft = new PointF(Center.X + lineLength, Center.Y + lineLength);
+            SizeF BoxSize = new SizeF(60.0F, 14.0F);
+            RectangleF InfoRect = new RectangleF(TopLeft, BoxSize);
+            dc.DrawLine(InfoPen, Center, TopLeft);
+            dc.DrawRectangle(InfoPen, InfoRect.X, InfoRect.Y, InfoRect.Width, InfoRect.Height);
+            dc.DrawString(body.name, new Font("Arial",10), new SolidBrush(System.Drawing.Color.Blue), TopLeft);
         }
 
         internal void renderCircle(Graphics dc, Pen pen, Vector position, double radius)
